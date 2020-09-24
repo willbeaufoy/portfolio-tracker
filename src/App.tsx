4a4057426234b74ba558712f1a8544c82e8c968e
Will Beaufoy <williambeaufoy@gmail.com>
2020-09-24 22:57:00 +0100

    Add demo HoldingForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HoldingForm from './HoldingForm';
 import {withAuthenticator} from '@aws-amplify/ui-react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Portfolio Tracker</header>
+      <HoldingForm></HoldingForm>
     </div>
   );
 }
