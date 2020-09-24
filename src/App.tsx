1730efc3e023596073ed94c66febb43a4b47cb04
Will Beaufoy <williambeaufoy@gmail.com>
2020-09-24 22:32:08 +0100

    Deploy and change title to portfolio tracker

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,7 @@ import {withAuthenticator} from '@aws-amplify/ui-react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Portfolio Tracker</header>
     </div>
   );
 }
