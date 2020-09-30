4516b2da591f7d8193e8922f6c1710267893e4a1
Will Beaufoy <williambeaufoy@gmail.com>
2020-09-30 11:36:09 +0100

    Get basic 'add trade' form working
    * Replace Formik UI date picker with react-datepicker as it's less intrusive.

--- src/AddTrade.tsx
+++ src/AddTrade.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import {Formik, Form, Field} from 'formik';
-import {MuiPickersUtilsProvider} from '@material-ui/pickers';
-import {DatePicker} from 'formik-material-ui-pickers';
-import DateFnsUtils from '@date-io/date-fns';
+import DatePicker from 'react-datepicker';
 import {API, graphqlOperation} from 'aws-amplify';
 import {createTrade} from './graphql/mutations';
 import {HoldingData} from './App';
 import './AddTrade.css';
+import 'react-datepicker/dist/react-datepicker.css';
 
 type AddTradeProps = {
   holding: HoldingData;
@@ -15,39 +14,49 @@ type AddTradeProps = {
 /** Form to add a trade. */
 export default function AddTrade(props: AddTradeProps) {
   return (
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <div>
-        <Formik
-          initialValues={{
-            date: new Date(),
-            shares: undefined,
-            price: undefined,
-          }}
-          onSubmit={(values, {setSubmitting}) => {
-            setTimeout(() => {
-              const input = {
-                holdingId: props.holding.id,
-                date: values.date.getTime(),
-                shares: values.shares,
-                price: values.price,
-              };
-              API.graphql(graphqlOperation(createTrade, {input}));
-              setSubmitting(false);
-            }, 400);
-          }}
-        >
-          {({isSubmitting}) => (
-            <Form className="AddTrade-Form">
-              <Field component={DatePicker} name="date" label="Date"></Field>
-              <Field type="number" name="shares" placeholder="Shares" />
-              <Field type="number" name="price" placeholder="Price" />
-              <button type="submit" disabled={isSubmitting}>
-                Add
-              </button>
-            </Form>
-          )}
-        </Formik>
-      </div>
-    </MuiPickersUtilsProvider>
+    <Formik
+      initialValues={{
+        date: new Date(),
+        shares: undefined,
+        price: undefined,
+      }}
+      onSubmit={(values, {setSubmitting}) => {
+        setTimeout(() => {
+          const input = {
+            holdingID: props.holding.id,
+            date: values.date.getTime(),
+            shares: values.shares,
+            price: values.price,
+            fee: 0,
+          };
+          console.log(input);
+          API.graphql(graphqlOperation(createTrade, {input}));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({isSubmitting, values, setFieldValue}) => (
+        <Form className="AddTrade-Form">
+          <div className="form-row">
+            <DatePicker
+              selected={values.date}
+              dateFormat="MMMM d, yyyy"
+              className="form-control"
+              name="date"
+              onChange={(date) => {
+                setFieldValue('date', date);
+              }}
+            />
+          </div>
+          <div className="form-row">
+            <Field type="number" name="shares" placeholder="Shares" />
+            <Field type="number" name="price" placeholder="Price" />
+            <button type="submit" disabled={isSubmitting}>
+              Add
+            </button>
+          </div>
+        </Form>
+      )}
+    </Formik>
   );
 }
