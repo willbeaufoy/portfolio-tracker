39b244103c605556d362df77eb0bec95fa2becaf
Will Beaufoy <williambeaufoy@gmail.com>
2020-10-09 22:44:40 +0100

    Update live backend URL to HTTPS version
    This should now work as I've added a self-signed SSL cert to the backend.

--- client/src/settings.ts
+++ client/src/settings.ts
@@ -1,7 +1,7 @@
 export const API_BASE =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8000/'
-    : 'http://portfolio-api.eba-ef93xgpm.us-west-2.elasticbeanstalk.com/';
+    : 'https://portfolio-api.eba-ef93xgpm.us-west-2.elasticbeanstalk.com/';
 
 export const MS_EOD_LATEST_BASE_URL =
   'https://api.marketstack.com/v1/eod/latest/';
