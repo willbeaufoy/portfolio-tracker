da2b5876661e999eccc0b480c79c45a254b4395b
Will Beaufoy <williambeaufoy@gmail.com>
2020-10-12 22:37:06 +0100

    Fix MSW handler so it gives the correct prices for stocks
    This is used for development and unit tests.

--- client/src/mocks/handlers.ts
+++ client/src/mocks/handlers.ts
@@ -1,26 +1,18 @@
 import {rest} from 'msw';
 import {MS_EOD_LATEST_BASE_URL} from '../settings';
 
-const holdingsData = {
-  data: [
-    {
-      symbol: 'AMZN',
-      close: 3482.74,
-    },
-    {
-      symbol: 'BOO.XLON',
-      close: 3.47,
-    },
-    {
-      symbol: 'TSLA',
-      close: 52.68,
-    },
-  ],
-};
+const holdingsBySymbol = new Map([
+  ['AMZN', {close: 3482.74}],
+  ['ASC.XLON', {close: 52.68}],
+  ['BOO.XLON', {close: 3.47}],
+  ['TSLA', {close: 442.3}],
+]);
 
 export const handlers = [
   rest.get(MS_EOD_LATEST_BASE_URL, (req, res, ctx) => {
-    return res(ctx.json(holdingsData));
+    const symbols = req.url.searchParams.get('symbols')!.split(',');
+    const data = symbols.map((s) => holdingsBySymbol.get(s) ?? {close: 0});
+    return res(ctx.json({data}));
   }),
   // rest.post('https://cognito-idp.eu-west-2.amazonaws.com/', (req, res, ctx) => {
   //   console.log('in cognito');
