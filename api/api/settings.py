3f753347e775fa5543691de9fe7bf2382c9c6fb0
Mike Gratton <62754870+mike100001@users.noreply.github.com>
2020-12-18 17:15:59 +0000

    Sort holdings when adding a new one (fixes #7)

--- api/api/settings.py
+++ api/api/settings.py
@@ -151,7 +151,7 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELIST = [
     'https://master.d2xlug9wi9lkht.amplifyapp.com',
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
 ]
 
 SESSION_COOKIE_SAMESITE = None
