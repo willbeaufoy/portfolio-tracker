ed8da041505a8f2d8d129aec85351be7faffc5cd
mike <mike@torque-gt.com>
2021-01-16 17:11:51 +0000

    Add 'last update time' for prices on frontend #37

--- client/src/holdings/HoldingsList.tsx
+++ client/src/holdings/HoldingsList.tsx
@@ -14,6 +14,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import DeleteIcon from '@material-ui/icons/Delete';
 import ShowChartIcon from '@material-ui/icons/ShowChart';
+import Tooltip from '@material-ui/core/Tooltip';
 
 import {API, FxRates, Holding, Performance, Trade, User} from '../api';
 import {USER_CURRENCY} from '../settings';
@@ -168,14 +169,21 @@ export function HoldingsList({user, fxRates}: IProps) {
             <div>Total Performance (incl. sales):</div>
             <PerformanceDisplay performance={totalPerformance} user={user} />
           </div>
-          <Button
-            variant='contained'
-            color='primary'
-            disabled={isRefreshing}
-            onClick={() => refreshPrices()}
-            aria-label='Refresh prices'>
-            Refresh Prices
-          </Button>
+          <Tooltip
+            title={
+              'Last updated at:' +
+              new Date(holdings[0]?.bidPriceUpdateTime).toLocaleString()
+            }
+            arrow>
+            <Button
+              variant='contained'
+              color='primary'
+              disabled={isRefreshing}
+              onClick={() => refreshPrices()}
+              aria-label='Refresh prices'>
+              Refresh Prices
+            </Button>
+          </Tooltip>
         </div>
       )}
       {Boolean(holdings.length) && (
