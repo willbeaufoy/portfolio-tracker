e656e15761e95379f0ee7e941cf2e86b3536e1a0
Mike Gratton <62754870+mike100001@users.noreply.github.com>
2021-01-17 22:13:20 +0000

    Add label to currency dropdown on 'add trade' dialog (fix #28) (#48)

--- client/src/holdings/AddTradeDialog.tsx
+++ client/src/holdings/AddTradeDialog.tsx
@@ -12,6 +12,7 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import {MuiPickersUtilsProvider} from '@material-ui/pickers';
+import Tooltip from '@material-ui/core/Tooltip';
 
 import {API} from '../api';
 import {CURRENCIES} from '../constants';
@@ -104,13 +105,19 @@ export function AddTradeDialog({holding, onTradeCreated}: AddTradeDialogProps) {
                 </Field>
                 <Field component={TextField} label='Broker' name='broker' />
                 <Field component={TextField} label='Quantity' name='quantity' />
-                <Field as='select' name='priceCurrency'>
-                  {CURRENCIES.map((c) => (
-                    <option key={c} value={c}>
-                      {c}
-                    </option>
-                  ))}
-                </Field>
+                <Tooltip
+                  title='This is the currency of the unit price of the trade'
+                  placement='top'
+                  arrow
+                  interactive>
+                  <Field as='select' name='priceCurrency'>
+                    {CURRENCIES.map((c) => (
+                      <option key={c} value={c}>
+                        {c}
+                      </option>
+                    ))}
+                  </Field>
+                </Tooltip>
                 <Field
                   component={TextField}
                   label={`Unit Price`}
