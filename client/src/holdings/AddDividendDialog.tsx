cd04e1e8a81165eac46dc630944175e9b3afac60
Mike Gratton <62754870+mike100001@users.noreply.github.com>
2021-02-16 23:20:55 +0000

    [Client] Change 'Add Dividend' dialog to use react-hook-form (#61)

--- client/src/holdings/AddDividendDialog.tsx
+++ client/src/holdings/AddDividendDialog.tsx
@@ -1,22 +1,25 @@
 import './AddTradeDialog.css';
 
-import {Field, Form, Formik} from 'formik';
-import {TextField} from 'formik-material-ui';
-import {KeyboardDateTimePicker} from 'formik-material-ui-pickers';
 import React, {useState} from 'react';
+import {Controller, useForm} from 'react-hook-form';
+import * as yup from 'yup';
 
 import DateFnsUtils from '@date-io/date-fns';
-import Button from '@material-ui/core/Button';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import {MuiPickersUtilsProvider} from '@material-ui/pickers';
+import {yupResolver} from '@hookform/resolvers/yup';
+import {
+    Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, TextField
+} from '@material-ui/core';
+import {DateTimePicker, MuiPickersUtilsProvider} from '@material-ui/pickers';
 
 import {API} from '../api';
 import {USER_CURRENCY} from '../settings';
 import {CreateDividendData, Holding} from '../types';
+
+interface IFormInput {
+  broker: string;
+  value: number;
+  date: Date;
+}
 
 export type IProps = {
   holding: Holding;
@@ -30,84 +33,105 @@ export function AddDividendDialog({
   showNotification,
 }: IProps) {
   const [open, setOpen] = useState(false);
+  const diviValidationSchema = yup.object().shape({
+    broker: yup.string(),
+    value: yup.number().required().moreThan(0),
+    date: yup.date(),
+  });
+  const {
+    register,
+    handleSubmit,
+    control,
+    errors,
+    formState: {isSubmitting},
+  } = useForm<CreateDividendData>({
+    resolver: yupResolver(diviValidationSchema),
+    defaultValues: {
+      broker: holding.trades.slice(-1)[0]?.broker ?? '',
+      value: undefined,
+      date: new Date().toISOString(),
+    },
+  });
 
-  function handleClickOpen() {
+  function openDialog() {
     setOpen(true);
   }
 
-  function handleCancel() {
+  function cancel() {
     setOpen(false);
+  }
+
+  async function submitForm(data: IFormInput) {
+    const createDividendData: CreateDividendData = {
+      holding: holding.id,
+      broker: data.broker ?? '',
+      value: data.value,
+      date: data.date.toISOString().split('.')[0].replace('T', ' '),
+    };
+    try {
+      const dividend = await API.createDividend(createDividendData);
+      onDividendCreated(dividend);
+      setOpen(false);
+    } catch (err) {
+      showNotification(
+        `Create dividend for ${holding.symbol} failed!`,
+        'error'
+      );
+    }
   }
 
   return (
     <div>
-      <Button variant='outlined' color='primary' onClick={handleClickOpen}>
+      <Button variant='outlined' color='primary' onClick={openDialog}>
         Add Dividend
       </Button>
-      <Dialog
-        open={open}
-        onClose={handleCancel}
-        aria-labelledby='form-dialog-title'>
-        <Formik
-          initialValues={{
-            date: new Date(),
-            broker: holding.trades.slice(-1)[0]?.broker ?? '',
-            value: '',
-          }}
-          onSubmit={async (values, {setSubmitting}) => {
-            const input: CreateDividendData = {
-              holding: holding.id,
-              date: values.date.toISOString().split('.')[0].replace('T', ' '),
-              broker: values.broker ?? '',
-              value: Number(values.value ?? 0),
-            };
-            try {
-              const dividend = await API.createDividend(input);
-              onDividendCreated(dividend);
-              setOpen(false);
-            } catch (err) {
-              showNotification(
-                `Create dividend for ${holding.symbol} failed!`,
-                'error'
-              );
-            }
-            setSubmitting(false);
-          }}>
-          {({isSubmitting}) => (
-            <Form>
-              <DialogTitle id='form-dialog-title'>
-                Add Dividend ({holding.symbol})
-              </DialogTitle>
-              <DialogContent className='DialogContent'>
-                <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                  <Field
-                    component={KeyboardDateTimePicker}
-                    label='Date'
+      <Dialog open={open} onClose={cancel} aria-labelledby='form-dialog-title'>
+        <form onSubmit={handleSubmit(submitForm)}>
+          <DialogTitle id='form-dialog-title' style={{textAlign: 'center'}}>
+            Add Dividend ({holding.symbol})
+          </DialogTitle>
+          <DialogContent className='DialogContent'>
+            <TextField
+              margin='dense'
+              label='Broker'
+              name='broker'
+              inputRef={register}
+            />
+            <TextField
+              margin='dense'
+              label={`Value (${USER_CURRENCY})`}
+              name='value'
+              inputRef={register}
+              error={!!errors.value}
+            />
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <Controller
+                name='date'
+                label='Date'
+                control={control}
+                render={({onChange, value}) => (
+                  <DateTimePicker
+                    value={value}
+                    onChange={(date) => onChange(date)}
                     variant='inline'
-                    name='date'
+                    inputRef={register}
                   />
-                </MuiPickersUtilsProvider>
-                <Field component={TextField} label='Broker' name='broker' />
-                <Field
-                  component={TextField}
-                  label={`Value (${USER_CURRENCY})`}
-                  name='value'
-                />
-              </DialogContent>
-              <DialogActions>
-                {!!isSubmitting && (
-                  <CircularProgress size={25} style={{marginRight: '10px'}} />
                 )}
-                <Button onClick={handleCancel} color='primary'>
-                  Cancel
-                </Button>
-                <Button type='submit' disabled={isSubmitting} color='primary'>
-                  Add
-                </Button>
-              </DialogActions>
-            </Form>
-          )}
-        </Formik>
+              />
+            </MuiPickersUtilsProvider>
+          </DialogContent>
+          <DialogActions>
+            {isSubmitting && (
+              <CircularProgress size={25} style={{marginRight: '10px'}} />
+            )}
+            <Button onClick={cancel} color='primary'>
+              Cancel
+            </Button>
+            <Button type='submit' disabled={isSubmitting} color='primary'>
+              Add
+            </Button>
+          </DialogActions>
+        </form>
       </Dialog>
     </div>
   );
